040000c34d8709575e687eab24846f85746e4278
suturbreverof <sfriedman6711@gmail.com>
2023-02-11 19:00:02 -0700

    working generate button, variables declared, function declared

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var withNumbers = "1234567890";
+var withSymbols = "!@#$%^&*()_+=<>;.";
+var withUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var withLowercase = "abcdefghijklmnopqrstuvwxyz";
+
+function generatePassword() {
+  var length = prompt("How many characters would you like your password to be? (minimum: 1, maximum: 128)");
+  var upcase = confirm("Would you like Upper Case Letters? (OK = Yes, Cancel = No)");
+  // var lowcase = confirm("Would you like Lower Case Letters? (OK = Yes, Cancel = No)");
+  // var symbols = confirm("Would you like symbols in your password? (OK = Yes, Cancel = No)");
+  // var numbers = confirm("Would you like numbers in your password? (OK = Yes, Cancel = No)");
+
+  if (upcase === true) {
+      var passcode = "";
+      for (i = 0; i < length; i++) {
+        passcode += withUppercase.charAt(
+          Math.floor(Math.random() * withUppercase.length)
+          );
+    }
+    return passcode;
+  }
+  console.log(passcode)
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
